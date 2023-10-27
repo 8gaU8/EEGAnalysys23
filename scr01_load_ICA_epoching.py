2e406bf44a40be88c7da769ca9548878fc25ab15
8gaU8 <haga.yuya2.2016@gmail.com>
2023-10-28 05:56:05 +0900

    add scripts

---
+++ scr01_load_ICA_epoching.py
@@ -0,0 +1,74 @@
+from message_senders import LineSender
+from rich.console import Console
+
+import eeg_utils
+from config import ICA_EPOCHS
+
+
+def save_and_notify(part_id, params, eegs, patch_func):
+    fname = ICA_EPOCHS / f"{part_id}-epo.fif.gz"
+    if fname.exists():
+        LineSender().send(part_id + " exists")
+        return
+
+    if patch_func is not None:
+        patch_func()
+        return
+
+    epochs = eeg_utils.calc_epochs(part_id, params, eegs)
+    epochs.save(fname)
+    LineSender().send(part_id)
+
+
+def get_m02_patch():
+    from patch_m04 import main
+
+    return main
+
+
+def main():
+    console = Console()
+    LineSender().send("開始します")
+
+    params = [1, 2, 3, 4, 5]
+    eegs = ["1", "2", "3", "4", "5"]
+    default_params = {"params": params, "eegs": eegs, "patch_func": None}
+
+    params_dict = {
+        "m01": default_params,
+        "m02": {
+            "params": params,
+            "eegs": ["1", "2", "3", "4", "5_re"],
+            "patch_func": None,
+        },
+        "m03": default_params,
+        "m04": default_params,
+        "m05": default_params,
+        "m06": default_params,
+        "nm01": default_params,
+        "nm02": default_params,
+        "nm03": {
+            "params": params,
+            "eegs": ["1_rem", "2", "3", "4", "5"],
+            "patch_func": None,
+        },
+        "nm04": {"params": params, "eegs": eegs, "patch_func": get_m02_patch()},
+        "nm06": default_params,
+    }
+    for part_id in params_dict.keys():
+        try:
+            save_and_notify(part_id=part_id, **params_dict[part_id])
+        except Exception as e:
+            LineSender().send("Exception at " + part_id)
+            console.print_exception(show_locals=True)
+            exit(code=1)
+
+    LineSender().send("終了しました")
+
+
+if __name__ == "__main__":
+    import os
+    from pathlib import Path
+
+    os.chdir(Path(__file__).parent)
+    main()
---
+++ scr02_calc_average_erp.py
@@ -0,0 +1,57 @@
+import mne
+from message_senders import LineSender
+from rich.console import Console
+
+from config import AVG_ERPS, ICA_EPOCHS
+
+console = Console()
+
+
+def load_and_calc_avg(part_id: str):
+    epochs = mne.read_epochs(ICA_EPOCHS / f"{part_id}-epo.fif.gz", verbose=False)
+    avg_move = epochs["move_probe_tone"].average()
+    avg_no_move = epochs["no_move_probe_tone"].average()
+    return avg_move, avg_no_move
+
+
+def calc_avg_epochs(part_ids: "list[str]", musicians: bool):
+    avg_move_list = []
+    avg_no_move_list = []
+    for part_id in part_ids:
+        LineSender().send(part_id)
+        try:
+            avg_move, avg_no_move = load_and_calc_avg(part_id)
+            avg_move_list.append(avg_move)
+            avg_no_move_list.append(avg_no_move)
+        except Exception:
+            LineSender().send("exception at " + part_id)
+            console.print_exception(show_locals=True)
+            exit(code=1)
+
+    fix = "m" if musicians else "nm"
+
+    avg_move = mne.combine_evoked(avg_move_list, weights="equal")
+    avg_move.save(AVG_ERPS / f"avg_move_{fix}-ave.fif.gz", overwrite=True)
+    avg_no_move = mne.combine_evoked(avg_no_move_list, weights="equal")
+    avg_no_move.save(AVG_ERPS / f"avg_no_move_{fix}-ave.fif.gz", overwrite=True)
+    return avg_move, avg_no_move
+
+
+def main():
+    # LineSender().send("musicians starts")
+    # part_ids = ["m01", "m02", "m03", "m04", "m05", "m06"]
+    # calc_avg_epochs(part_ids=part_ids, musicians=True)
+
+    LineSender().send("no musicians starts")
+    part_ids = ["nm01", "nm02", "nm03", "nm04"]
+    calc_avg_epochs(part_ids=part_ids, musicians=False)
+
+    LineSender().send("終了しました")
+
+
+if __name__ == "__main__":
+    import os
+    from pathlib import Path
+
+    os.chdir(Path(__file__).parent)
+    main()
