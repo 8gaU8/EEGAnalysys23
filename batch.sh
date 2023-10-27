5b33c581af2ffe3bede8c8c0de1eeb4b445dc50f
8gaU8 <haga.yuya2.2016@gmail.com>
2023-10-28 06:06:27 +0900

    ファイル名変更に対応

--- batch.sh
+++ batch.sh
@@ -6,7 +6,8 @@ err() {
   echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $*" >&2
 }
 
-/opt/homebrew/Caskroom/mambaforge/base/envs/NIDlab/bin/python 01_load_ICA_epoching.py
-/opt/homebrew/Caskroom/mambaforge/base/envs/NIDlab/bin/python 02_calc_average_erp.py
+### run python scripts
+/opt/homebrew/Caskroom/mambaforge/base/envs/NIDlab/bin/python scr01_load_ICA_epoching.py
+/opt/homebrew/Caskroom/mambaforge/base/envs/NIDlab/bin/python scr02_calc_average_erp.py
 
 
