a1c9d527ff99918ef830bafd55019361d93a4534
8gaU8 <haga.yuya2.2016@gmail.com>
2023-10-30 16:33:34 +0900

    パス取得関数追加

--- config.py
+++ config.py
@@ -1,12 +1,34 @@
 from pathlib import Path
+from typing import Literal
 
-_TMP_FILE_DIR = Path("/Volumes/data/haga/data/inter_files")
+_INTER_FILES_ROOT = Path("/Volumes/data/haga/data/inter_files")
 
-ICA_EPOCHS = _TMP_FILE_DIR / "ica_epochs"
-AVG_ERPS = _TMP_FILE_DIR / "avg_erps"
+ICA_EPOCHS_DIR = _INTER_FILES_ROOT / "ica_epochs"
+AVG_ERPS_DIR = _INTER_FILES_ROOT / "avg_erps"
+ERP_CSVS_DIR = _INTER_FILES_ROOT / "erp_csvs"
 
-
-_inter_dirs = [ICA_EPOCHS, AVG_ERPS]
+_inter_dirs = [ICA_EPOCHS_DIR, AVG_ERPS_DIR, ERP_CSVS_DIR]
 for dir in _inter_dirs:
     if not dir.exists():
         dir.mkdir(parents=True)
+
+
+def ica_epoch(part_id: str) -> Path:
+    path = ICA_EPOCHS_DIR / f"{part_id}-epo.fif.gz"
+    return path
+
+
+def evoked(
+    group: Literal["m", "nm"],
+    condition: Literal["move", "no_move"],
+) -> Path:
+    path = AVG_ERPS_DIR / f"avg_{condition}_{group}-ave.fif.gz"
+    return path
+
+
+def erp_pkls(
+    part_id: str, condition: Literal["move", "no_move"], center: float
+) -> Path:
+    center = int(center * 1e3)
+    path = ERP_CSVS_DIR / f"erp_{part_id}_{condition}_{center}-erp.pkl"
+    return path
