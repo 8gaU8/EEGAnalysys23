f5a83d4d620560747eaa83b2db36c8d814aa7124
8gaU8 <haga.yuya2.2016@gmail.com>
2023-11-15 11:22:48 +0900

    ディレクトリ校正変更

--- config.py
+++ config.py
@@ -1,10 +1,13 @@
+from datetime import datetime
 from pathlib import Path
-from typing import Literal, NamedTuple
+from typing import Literal, NamedTuple, Tuple
+
+import mne
 
 
 #######* CONSTS
 class _consts(NamedTuple):
-    NB_BIN = 7
+    NB_BIN = 9
     MOVE_MSG = 100
     DONT_MOVE_MSG = 200
     DELAY_BIN_ORDER = 1000
@@ -45,18 +48,32 @@ for k, v in _base_event_dict.items():
 
 #######* path
 
-_INTER_FILES_ROOT = Path("/Volumes/data/haga/data/inter_files_1109")
+_INTER_FILES_ROOT = Path("/Volumes/data/haga/data/inter_files_1110")
+_INTER_FILES_ROOT = Path("/Volumes/data/haga/data/inter_files_1114")
 
 
 ICA_EPOCHS_DIR = _INTER_FILES_ROOT / "ica_epochs"
 AVG_ERPS_DIR = _INTER_FILES_ROOT / "avg_erps"
 ERP_CSVS_DIR = _INTER_FILES_ROOT / "erp_csvs"
 FIG_DIR = Path("../figs")
-
-_inter_dirs = [ICA_EPOCHS_DIR, AVG_ERPS_DIR, ERP_CSVS_DIR, FIG_DIR]
+ERP_CSVS_PER_BIN_DIR = _INTER_FILES_ROOT / "erp_csvs_per_bin"
+LOG_DIR = _INTER_FILES_ROOT / "logs"
+_inter_dirs = [
+    ICA_EPOCHS_DIR,
+    AVG_ERPS_DIR,
+    ERP_CSVS_DIR,
+    FIG_DIR,
+    ERP_CSVS_PER_BIN_DIR,
+    LOG_DIR,
+]
 for dir in _inter_dirs:
     if not dir.exists():
         dir.mkdir(parents=True)
+
+
+LOG_FILE = LOG_DIR / datetime.now().strftime("%y%m%d-%H%M%S.log")
+
+mne.set_log_file(LOG_FILE)
 
 
 def ica_epoch_path(part_id: str) -> Path:
@@ -77,4 +94,14 @@ def erp_pkls(
 ) -> Path:
     center = int(center * 1e3)
     path = ERP_CSVS_DIR / f"erp_{part_id}_{condition}_{center}-erp.pkl"
+    return path
+
+
+def erp_pkls_per_bin(
+    part_id: str, conditions: list[str], bins: list[str], center: float
+) -> Path:
+    center_s = str(int(center * 1e3))
+    bins_s = "".join(bins)
+    cond_s = "_".join(conditions)
+    path = ERP_CSVS_PER_BIN_DIR / f"erp_{part_id}_{cond_s}_{bins_s}_{center_s}-erp.pkl"
     return path
